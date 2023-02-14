cd4785757669c7779bbebe4ec5cfb4c1b846549b
junhwan <kjunhwan1998@gmail.com>
2023-02-15 06:59:01 +0900

    feat:로그인 작업 전 업로드

--- src/login/login_form.jsx
+++ src/login/login_form.jsx
@@ -2,7 +2,18 @@ import { CheckBtn } from "../common/button";
 import style from "./login.module.css";
 import sample from "../asset/images/home_logo.png";
 import { LoginInput } from "./login_input";
+import axios from 'axios';
 
+function searchApi() {
+  const url = "http://127.0.0.1:8000/accounts/registration";
+  axios.post(url)
+  .then(function(response) {
+      console.log("성공");
+  })
+  .catch(function(error) {
+      console.log("실패");
+  })
+}
 function LoginPage(prob) {
   return (
     <div className={style.login_form}>
--- src/main/main.jsx
+++ src/main/main.jsx
@@ -5,6 +5,9 @@ import NavBtn from "../common/nav";
 function MainPage() {
   return (
     <div className={style.main_page}>
+      <h2>인기 매점</h2>
+      <SimpleSlider className={style.store} />
+      <h2>뉴스 기사</h2>
       <SimpleSlider />
       <NavBtn />
     </div>
