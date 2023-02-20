3f21823eb309deb9b764e74a464ff479570dd07b
junhwan <kjunhwan1998@gmail.com>
2023-02-21 00:44:40 +0900

    feat:signup_post

--- src/login/login_form.jsx
+++ src/login/login_form.jsx
@@ -1,19 +1,18 @@
-import { CheckBtn } from "../common/button";
-import style from "./login.module.css";
-import sample from "../asset/images/home_logo.png";
-import { LoginInput } from "./login_input";
-import axios from 'axios';
+// import { CheckBtn } from "../common/button";
+// import style from "./login.module.css";
+// import sample from "../asset/images/home_logo.png";
+// import { LoginInput } from "./login_test";
+// import axios from "axios";
 
-
-function LoginPage(prob) {
-  return (
-    <div className={style.login_form}>
-      <img className={style.login_form_img} src={sample} alt="" />
-      <LoginInput className={style.pillBtn} content="ID" />
-      <LoginInput content="Password" />
-      <CheckBtn check="로그인" />
-    </div>
-  );
-}
+// function LoginPage(prob) {
+//   return (
+//     <div className={style.login_form}>
+//       <img className={style.login_form_img} src={sample} alt="" />
+//       <LoginInput className={style.pillBtn} content="ID" />
+//       <LoginInput content="Password" />
+//       <CheckBtn check="로그인" />
+//     </div>
+//   );
+// }
 
 // export { LoginPage };
